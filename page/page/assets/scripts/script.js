184dc35d4bd3cad2a34508c7e44eecf9a2bfddd8
Romasch <romashmlc@gmail.com>
2023-06-20 13:53:41 +0300

    upd

---
+++ page/page/assets/scripts/script.js
@@ -0,0 +1,9 @@
+// $("#prev-1").on('click', function() {
+//     $("#face-1").attr('src') = "assets/models/face/scene.gltf";
+//     $("#prev-1").attr('id') = 'prev-2';
+// });
+
+// $("#prev-2").on('click', function() {
+//     $("#face-1").attr('src') = "assets/models/pimps/pimps-face.gltf";
+//     $("#prev-2").attr('id') = 'prev-1';
+// });
